2707124be2174e07b977c4efee0da9ec9ea16b56
Subhashini Raghunathan <shubi4@hotmail.com>
2016-12-11 20:47:03 -0800

    inference

--- languagemodel/config.py
+++ languagemodel/config.py
@@ -2,7 +2,7 @@ TRAIN_FILES = "/w266/project/mscoco/image_vectors_and_captions/train-?????-of-00
 VAL_FILES = "/w266/project/mscoco/image_vectors_and_captions/val-?????-of-00004"
 TEST_FILES = "/w266/project/mscoco/image_vectors_and_captions/test-?????-of-00008"
 
-CHECKPOINT_DIR = "/w266/project/mscoco/model_checkpoint"
+CHECKPOINT_DIR = "/w266/project/mscoco/rnn_model_checkpoints"
 MAX_CHECKPOINTS_TO_KEEP=5
 
 EVAL_DIR="/w266/project/mscoco/model_eval"
@@ -19,6 +19,11 @@ BATCH_SIZE = 32
 LOG_EVERY_N_STEPS = 1
 NUMBER_OF_STEPS = 1000000
 
-VOCAB_FILE=""
-INFERENCE_FILES=""
+#VOCAB_FILE="/home/subhashini.r/mscoco/annotations/word_counts.txt"
+VOCAB_FILE="/w266/project/mscoco/captions_train_val2014/annotations/word_counts.txt"
 
+INFERENCE_FILES=""
+#INFERENCE_IMAGE_FILES_DIR="/home/subhashini.r/mscoco/images/validation/label"
+INFERENCE_IMAGE_FILES_DIR="/w266/project/mscoco/debug/validation/label"
+#INFERENCE_VECTOR_FILES_DIR="/home/subhashini.r/mscoco/inception_image_vectors/validation"
+INFERENCE_VECTOR_FILES_DIR="/w266/project/mscoco/debug/image_vectors/validation"
