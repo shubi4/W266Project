3751ed960f0b93ffe3a6617777d1b41abe14ce01
Subhashini Raghunathan <shubi4@hotmail.com>
2016-12-14 23:27:13 -0800

    inference

--- languagemodel/config.py
+++ languagemodel/config.py
@@ -1,15 +1,17 @@
-TRAIN_FILES = "/home/subhashini.r/mscoco/image_vectors_and_captions/train-?????-of-00256"
-VAL_FILES = "/w266/project/mscoco/image_vectors_and_captions/val-?????-of-00004"
-TEST_FILES = "/w266/project/mscoco/image_vectors_and_captions/test-?????-of-00008"
-PROCESSED_TEST_FILE="/w266/project/mscoco/predictions/test_data.json"
+#BASE_PATH="/home/subhashini.r/"
+BASE_PATH="/w266/project/"
+TRAIN_FILES = BASE_PATH + "mscoco/image_vectors_and_captions/train-?????-of-00256"
+VAL_FILES = BASE_PATH + "mscoco/image_vectors_and_captions/val-?????-of-00004"
+TEST_FILES = BASE_PATH + "mscoco/image_vectors_and_captions/test-?????-of-00008"
+PROCESSED_TEST_FILE= BASE_PATH + "mscoco/predictions/test_data.json"
 
 #folder to pick up checkpoint for re-training
-CHECKPOINT_DIR = "/home/subhashini.r/mscoco/rnn_model_checkpoint"
+CHECKPOINT_DIR = BASE_PATH + "mscoco/rnn_model_checkpoint"
 #checkpoint file to pick up for evaluation and inference
-CHECKPOINT_FILE = "/w266/project/mscoco/rnn_model_checkpoints/model.ckpt-171230"
+CHECKPOINT_FILE = BASE_PATH + "mscoco/rnn_model_checkpoints/model.ckpt-171230"
 MAX_CHECKPOINTS_TO_KEEP=200
 
-EVAL_DIR="/w266/project/mscoco/rnn_model_eval"
+EVAL_DIR= BASE_PATH + "mscoco/rnn_model_eval"
 EVAL_INTERVAL_SECS=600 # "Interval between evaluation runs."
 NUM_EVAL_EXAMPLES=10132
 MIN_GLOBAL_STEP=5000 # "Minimum global step to run evaluation."
@@ -24,9 +26,7 @@ LOG_EVERY_N_STEPS = 50
 NUMBER_OF_STEPS = 1000000
 
 #VOCAB_FILE="/home/subhashini.r/mscoco/annotations/word_counts.txt"
-VOCAB_FILE="/w266/project/mscoco/annotations/word_counts.txt"
+VOCAB_FILE= BASE_PATH + "mscoco/annotations/word_counts.txt"
 
-#INFERENCE_IMAGE_FILES_DIR="/home/subhashini.r/mscoco/images/validation/label"
-INFERENCE_IMAGE_FILES_DIR="/w266/project/mscoco/images/validation/label"
-#INFERENCE_VECTOR_FILES_DIR="/home/subhashini.r/mscoco/inception_image_vectors/validation"
-INFERENCE_VECTOR_FILES_DIR="/w266/project/mscoco/inception_image_vectors/validation"
+INFERENCE_IMAGE_FILES_DIR= BASE_PATH + "mscoco/images/validation/label"
+INFERENCE_VECTOR_FILES_DIR= BASE_PATH + "mscoco/inception_image_vectors/validation"
--- languagemodel/inference.py
+++ languagemodel/inference.py
@@ -2,13 +2,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import math
 import os.path
 import time
 import sys
 import numpy as np
 import tensorflow as tf
 import json
+import nltk
 
 from config import *
 from rnnlm import LanguageModel
@@ -17,7 +17,7 @@ from collections import defaultdict
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-def ExtractTestData(file_pattern, output_file):
+def ExtractData(file_pattern, output_file):
     data_files = []
     for pattern in file_pattern.split(","):
         data_files.extend(tf.gfile.Glob(pattern))
@@ -40,7 +40,8 @@ def ExtractTestData(file_pattern, output_file):
                 caption = sess.run(caption)
                 id = sess.run(image_id)
                 image_filename = "COCO_val2014_%012d.jpg" %id
-                images_to_captions[image_filename].append(" ".join(caption))
+                images_to_captions[image_filename].append(list(caption))
+                print(images_to_captions[image_filename])
         print("writing data to file")
         with open(output_file, 'w') as f:
             json.dump(images_to_captions, f)
@@ -48,28 +49,14 @@ def ExtractTestData(file_pattern, output_file):
 
 def DoInference(input_file, output_file):
     with open(input_file, 'r') as f:
-        images_and_captions = json.load(f)
+        ground_truth = json.load(f)
 
-
-    image_predictions = defaultdict(lambda: [])
+    predictions = defaultdict(lambda: [])
     vocab = Vocabulary(VOCAB_FILE)
 
-
-    vector_filename = os.path.join(INFERENCE_VECTOR_FILES_DIR, filename)
-    #image_filename = os.path.join(INFERENCE_IMAGE_FILES_DIR, sys.argv[1])
-    image_vector = np.fromfile(vector_filename, dtype=np.float32)
-    print(image_vector.shape)
-    assert image_vector.shape == (2048,)
-
-
-
-    model_path = tf.train.latest_checkpoint(CHECKPOINT_DIR)
-    if not model_path:
-        tf.logging.info("Skipping evaluation. No checkpoint found in: %s", CHECKPOINT_DIR)
-        return
-
     g = tf.Graph()
-    with g.as_default():
+    with g.as_default(), tf.Session() as sess:
+        model_path = CHECKPOINT_FILE
         # Build the model for evaluation.
         model = LanguageModel(mode="inference")
         model.BuildCoreGraph()
@@ -78,25 +65,39 @@ def DoInference(input_file, output_file):
         # Create the Saver to restore model Variables.
         saver = tf.train.Saver()
 
-        with tf.Session() as sess:
-            # Load model from checkpoint.
-            tf.logging.info("Loading model from checkpoint: %s", model_path)
-            saver.restore(sess, model_path)
-            global_step = tf.train.global_step(sess, model.global_step_.name)
-            tf.logging.info("Successfully loaded %s at global step = %d.", os.path.basename(model_path), global_step)
+        # Load model from checkpoint.
+        tf.logging.info("Loading model from checkpoint: %s", model_path)
+        saver.restore(sess, model_path)
+        global_step = tf.train.global_step(sess, model.global_step_.name)
+        tf.logging.info("Successfully loaded %s at global step = %d.", os.path.basename(model_path), global_step)
 
-            caption = []
+        for imagefile, captions in ground_truth:
+            vector_filename = os.path.join(INFERENCE_VECTOR_FILES_DIR, imagefile)
+            image_vector = np.fromfile(vector_filename, dtype=np.float32)
+            #print(image_vector.shape)
+            assert image_vector.shape == (2048,)
+
+            captionids = []
             h = sess.run(model.initial_h_, feed_dict = {model.image_feed_: image_vector})
             wordid = np.array([vocab.start_id])
 
-            caption.append(wordid[0])
+            captionids.append(wordid[0])
             for i in xrange(50):
                 feed = {model.initial_h_: h, model.input_wordid_: wordid}
                 wordid, h = sess.run([model.pred_samples_, model.final_h_], feed_dict=feed)
-                caption.append(wordid[0])
+                captionids.append(wordid[0])
                 if wordid[0] == vocab.end_id:
                     break
 
+            predicted_caption = [vocab.id_to_word(word_id) for word_id in captionids]
+            predictions[imagefile].append(" ".join(predicted_caption))
+
+            for ref in ground_truth[imagefile]:
+                ref = ref.split(" ")
+            # there may be several references
+            #BLEUscore = nltk.translate.bleu_score.sentence_bleu([reference], hypothesis)
+            #print
+            #BLEUscore
             #print(caption)
             print("Predicted caption: "),
             print(" ".join(vocab.id_to_word(word_id) for word_id in caption))
@@ -105,8 +106,10 @@ def DoInference(input_file, output_file):
 
 def main(unused_argv):
     if not os.path.isfile(PROCESSED_TEST_FILE):
+        print("extracting image filenames and captions from %s into %s" %(TEST_FILES,PROCESSED_TEST_FILE))
         ExtractData(TEST_FILES, PROCESSED_TEST_FILE)
 
+    DoInference(PROCESSED_TEST_FILE, PREDICTIONS_FILE)
 
 if __name__ == "__main__":
     tf.app.run()
